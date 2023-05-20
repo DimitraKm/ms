7c237c0d832b5be21a01e669ab60b09aeffa9539
DimitraKm <d.kontaki13@gmail.com>
2023-05-20 11:03:17 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System.IO.Pipes;
+
+namespace ms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            school s1 = new school("fytuguy", 23);
+            s1.search("f", 120);
+            
+        }
+    }
+}
---
+++ school.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ms
+{
+    internal class school
+    {
+
+        private string name;
+        private int age;
+        public string person
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        
+       public school(string name, int age)
+       {
+          this.name = name;
+           this.age = age;
+
+          
+       }
+        public void search(string name, int age)
+        {
+            if (name == "m" && age >= 16)
+            {
+                Console.WriteLine("Mr.");
+            }
+            if (name == "m" && age < 16)
+            {
+                Console.WriteLine("Master");
+            }
+            if (name == "f" && age >= 16)
+            {
+                Console.WriteLine("Ms.");
+            }
+            if (name == "f" && age < 16)
+            {
+                Console.WriteLine("Miss");
+            }
+        }
+
+    }
+}
